f680fcf18931f7bb3fa9810d5b03e90011d96f3b
Satyanchal <62104921+Bluehatcoders@users.noreply.github.com>
2021-04-04 10:46:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from "react"
-import { Link } from 'react-router-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Body from './Body';
-import Setting from './Setting';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <BrowserRouter> 
-   <Route exact path="/" component={Body} />
-   <Route exact path="/setting" component={Setting} />
-    </BrowserRouter>
+    
 </div>
     );
   }
